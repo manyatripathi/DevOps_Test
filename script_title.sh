013664b778b1f5cf46714506bb9736ee34733f3f
manyatripathi <49054370+manyatripathi@users.noreply.github.com>
2021-05-24 18:43:29 +0530

    Update script_title.sh

--- script_title.sh
+++ script_title.sh
@@ -2,15 +2,12 @@
 
 echo "Starting the script to update title"
 
-whoami
-
-pwd
-id -nG
-
-docker ps
-
-
 sed -i "s/<title>.*<\/title>/<title>$1<\/title>/g" index.html
+
+if [ ! "$(docker ps -q -f name=custom_nginx)" ]; then
+  echo "Deleting existing container"
+  docker rm -f custom_nginx
+fi
 
 docker-compose  up -d --build
 
